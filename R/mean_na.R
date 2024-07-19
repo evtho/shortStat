5c02883b77cf91ad41389dc470795248c6748e05
Eva Thoma <eva.thoma98@gmail.com>
2024-07-19 13:32:58 +0200

    shortend code

--- R/mean_na.R
+++ R/mean_na.R
@@ -9,6 +9,7 @@
 #' mean_na(c(1, 2, "a", NA, 4, 5, "b", 5))
 mean_na <- function(x) {
   numeric_values <- as.numeric(x)
-  numeric_values <- numeric_values[!is.na(numeric_values)]
   return(mean(numeric_values, na.rm = TRUE))
 }
+
+
